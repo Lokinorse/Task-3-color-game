545ea5a9758e344a3b9028df90149ff151000e8b
Ratibor <demonictype@gmail.com>
2019-06-14 02:09:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-/* !!!Задание недоделано. Успел сделать только таймер!!! */
-var board = [];
-var columns = [1, 2, 3, 4];
-//Stopwatch:
-var seconds = 0;
-var minutes = 0;
-var hours = 0;
+let board = [];
+let pairs = [];
+let columns = [1, 2, 3, 4];
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
 
-function setColors(){
-    board.forEach(function(el){
-/*         return el.color='black' */
-    })
-}
 
-
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: function () {
-        for (i = 0; i < 4; i++) {
-            for (l = 0; l < 4; l++) {
-                board.push({ x: i, y: l })
-            }
+        const colorBox = ['red', 'blue', 'black', 'green', 'orange', 'violet', 'chocolate', 'white']
+        //Добавляем ещё по паре цветов
+        colorBox.map(function (el) {
+            colorBox.push(el);
+        })
+        let varColor = function () {
+            let random = [Math.floor(Math.random() * colorBox.length)]
+            let chosenColor = colorBox[random];
+            colorBox.splice(random, 1);
+            return chosenColor
         }
+        for (i = 0; i < 16; i++) {
+            board.push({ ind: i, color: varColor(), head: true })
+        }
+
+
         return {
             board,
-            colorBox:['red','blue','white','green','orange','red','chocolate'] ,
+            stopWatchIsOn: false,
+            counter: 0,
+            indexSequence: [],
             stopWatchData: "00:00:00",
+
         }
     },
+
     methods: {
+
         stopWatch: function () {
+            app.stopWatchIsOn = true;
             setInterval(function add() {
-/*                 Каждую секунду мы будем прибавлять по одной секунде, пока их не станет более 59, тогда мы прибавим минуту, а секунды обнулим */
+                /*Каждую секунду мы будем прибавлять по одной секунде, пока их не станет более 59, тогда мы прибавим минуту, а секунды обнулим */
                 seconds++;
                 if (seconds >= 60) {
                     seconds = 0;
@@ -41,13 +52,58 @@ var app = new Vue({
                         hours++;
                     }
                 }
-/*                 Создаём формат времени с двумя нулями (00:00:00) */
+                /* Создаём формат времени с двумя нулями (00:00:00) */
                 app.stopWatchData = (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
                     ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
                     ":" + (seconds > 9 ? seconds : "0" + seconds);
             }, 1000)
+        },
+        turnIt: function (e) {
+            if (app.stopWatchIsOn == true) {
+                app.counter += 1;
+                /*             При нажатии на клетку меняет значение св-ва head на false,
+                            чтобы появился цвет клетки. */
+                let currentIndex = board[e.target.innerHTML].ind;
+                if (app.counter < 3) {
+                    app.board[currentIndex].head = false;
+                }
+                /*             Добавляем цвет клетки, на которую только что нажали в массив,
+                            colorSequence, чтобы иметь возможность открыть угаданные клетки. */
+
+                app.indexSequence.push(currentIndex)
+                let currentColor = app.board[currentIndex].color
+                let previousColor = app.board[app.indexSequence[app.indexSequence.length - 2]].color;
+
+                if (app.counter == 2) {
+                    app.counter = 0;
+                /* Если игроку удалось последовательно открыть две клетки определённого цвета, то закрашиваем их 
+                с помощью св-ва head. */
+                    if (previousColor !== currentColor) {
+/*                         Используем setTimeout, чтобы отобразить юзеру цвет клетки, в случае, если он не угадал. */
+                        setTimeout(function () {
+                            app.board[currentIndex].head = true;
+                            app.board[app.indexSequence[app.indexSequence.length - 2]].head = true;
+
+                        }, 200)
+                    }
+
+                }
+
+                if (app.counter > 2) {
+                    app.counter = 0;
+                }
+
+            }
+            function areAllHeadsTrue(el) {
+                return el.head === false
+            }
+/*             Если все клетки закрашены (имеют св-во head = true), то игрок победил, показываем alert */
+            if (app.board.every(areAllHeadsTrue)) {
+                alert('Вы выиграли!\n Затраченное время:' + app.stopWatchData)
+            }
 
         }
     },
+
 
 });
